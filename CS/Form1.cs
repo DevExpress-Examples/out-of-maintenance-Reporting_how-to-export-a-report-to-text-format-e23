24d9aa69019e7a259e3f8a1a5f51e181fbe88282
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Text;
+using System.Diagnostics;
+using System.Globalization;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+// ...
+
+
+namespace ExportToTextCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // A path to export a report.
+            string reportPath = "c:\\Test.txt";
+
+            // Create a report instance.
+            XtraReport1 report = new XtraReport1();
+
+            // Get its Text export options.
+            TextExportOptions txtOptions = report.ExportOptions.Text;
+
+            // Set Text-specific export options.
+            txtOptions.Encoding = Encoding.Unicode;
+            txtOptions.Separator = CultureInfo.CurrentCulture.TextInfo.ListSeparator.ToString();
+
+            // Export the report to Text.
+            report.ExportToText(reportPath);
+
+            // Show the result.
+            StartProcess(reportPath);
+        }
+
+        // Use this method if you want to automaically open
+        // the created Text file in the default program.
+        public void StartProcess(string path)
+        {
+            Process process = new Process();
+            try
+            {
+                process.StartInfo.FileName = path;
+                process.Start();
+                process.WaitForInputIdle();
+            }
+            catch { }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to export a report to Text format
+
+
+<p>The following example demonstrates how to export a report to Text format. To do this, the XtraReport.ExportToText method should be used. It also demonstrates what specific TextExportOptions may be used when a report is exported to Text.</p>
+
+<br/>
+
+
